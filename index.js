ee29a09371d9ec0fed434d2aa5ed80448de613a5
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 16:03:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function getRepositories() {
     req.open("GET", "https://api.github.com/users/yoheezus/repos")
     req.send()
 }
+
+
+function showRepositories(event, data) {
+    console.log(this.responseText)
+}
