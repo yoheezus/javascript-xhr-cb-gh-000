6dce1a294284c9a1f0d62c3ff0902502e54b4675
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-02-02 16:33:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function showRepositories(event, data) {
 }
 
 function getCommits(el) {
-    const name = el.dataset.repoList
+    const name = el.dataset.repo
     const req = new XMLHttpRequest()
     req.addEventListener("load", showCommits);
     req.open("GET", "https://api.github.com/repos/yoheezus/" + name + "/commits")
